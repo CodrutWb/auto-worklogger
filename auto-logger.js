31536cbc3bab4261204e2c546077627ca0863b2c
Codrut Dica <codrut.dica@webitfactory.io>
2025-03-03 11:43:39 +0200

    Updated auto-logger.js

--- auto-logger.js
+++ auto-logger.js
@@ -127,7 +127,7 @@ async function submitWorklog() {
 function scheduleCron() {
   try {
     console.log("Initializing cronjob!");
-    cron.schedule('32 16 * * 1-5', submitWorklog); // This will run Mon-Fri at 11 PM
+    cron.schedule('50 09 * * 1-5', submitWorklog); // This will run Mon-Fri at 11 PM
     console.log("Cronjob initialized successfully!")
   }
   catch(e) {
